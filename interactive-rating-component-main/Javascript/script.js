30500ed44ee4242abd61e8f127337c39ff35455d
Ratnesh sahu <69114831+alexmercer500@users.noreply.github.com>
2022-04-11 00:17:52 +0530

    Add files via upload

---
+++ interactive-rating-component-main/Javascript/script.js
@@ -0,0 +1,37 @@
+const ratingStar = document.querySelectorAll(".rate");
+const submitButton = document.getElementById("btn");
+const ratingCard = document.querySelector(".box1");
+const thanksCard = document.querySelector(".box2");
+let rateValue = false;
+const errorMsg = document.querySelector("h5");
+const ratingValue = document.querySelector(".rate-me");
+
+ratingStar.forEach(star => {
+    star.addEventListener("click", () => {
+        if (star.classList.contains("rated")) {
+            star.classList.remove("rated");
+            rateValue = false;
+        }
+        else {
+            ratingStar.forEach(stared => {
+                stared.classList.remove("rated")
+                rateValue = false;
+            })
+            star.classList.add("rated");
+            rateValue = true;
+        }
+    })
+    submitButton.addEventListener("click", () => {
+        if (rateValue) {
+            ratingCard.style.display = "none";
+            thanksCard.style.display = "block";
+            ratingValue.innerHTML = `You selected ${star.innerHTML} out of 5`
+        }
+        else {
+            errorMsg.innerText = "Please rate before submission";
+            setTimeout(() => {
+                errorMsg.innerText = "";
+            }, 4000);
+        }
+    })
+});
