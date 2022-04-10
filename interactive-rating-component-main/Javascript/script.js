eb0d8a9e3e942ef2cebb25549d37704929835fa9
Ratnesh sahu <69114831+alexmercer500@users.noreply.github.com>
2022-04-11 00:44:39 +0530

    Add files via upload

--- interactive-rating-component-main/Javascript/script.js
+++ interactive-rating-component-main/Javascript/script.js
@@ -5,6 +5,7 @@ const thanksCard = document.querySelector(".box2");
 let rateValue = false;
 const errorMsg = document.querySelector("h5");
 const ratingValue = document.querySelector(".rate-me");
+let totalStar;
 
 ratingStar.forEach(star => {
     star.addEventListener("click", () => {
@@ -19,13 +20,14 @@ ratingStar.forEach(star => {
             })
             star.classList.add("rated");
             rateValue = true;
+            totalStar = star.innerHTML;
         }
     })
     submitButton.addEventListener("click", () => {
         if (rateValue) {
             ratingCard.style.display = "none";
             thanksCard.style.display = "block";
-            ratingValue.innerHTML = `You selected ${star.innerHTML} out of 5`
+            ratingValue.innerHTML = `You selected ${totalStar} out of 5`
         }
         else {
             errorMsg.innerText = "Please rate before submission";
